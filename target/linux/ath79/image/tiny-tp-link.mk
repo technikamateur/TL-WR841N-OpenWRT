950e9ca0bbaa344a26a857ed3acff88008538ed0
Daniel <daniel@technikamateur.de>
2025-02-17 18:50:19 +0100

    added conf for v10, v11, v12

--- target/linux/ath79/image/tiny-tp-link.mk
+++ target/linux/ath79/image/tiny-tp-link.mk
@@ -8,3 +8,39 @@ define Device/tplink_tl-wr841-v9
   SUPPORTED_DEVICES += tl-wr841n-v9
 endef
 TARGET_DEVICES += tplink_tl-wr841-v9
+
+define Device/tplink_tl-wr841-v10
+  $(Device/tplink-16mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR841N/ND
+  DEVICE_VARIANT := v10
+  TPLINK_HWID := 0x08410010
+  SUPPORTED_DEVICES += tl-wr841n-v9
+endef
+TARGET_DEVICES += tplink_tl-wr841-v10
+
+define Device/tplink_tl-wr841-v11
+  $(Device/tplink-16mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR841N/ND
+  DEVICE_VARIANT := v11
+  TPLINK_HWID := 0x08410011
+  SUPPORTED_DEVICES += tl-wr841n-v11
+  IMAGES += factory-us.bin factory-eu.bin
+  IMAGE/factory-us.bin := tplink-v1-image factory -C US
+  IMAGE/factory-eu.bin := tplink-v1-image factory -C EU
+endef
+TARGET_DEVICES += tplink_tl-wr841-v11
+
+define Device/tplink_tl-wr841-v12
+  $(Device/tplink-16mlzma)
+  SOC := qca9533
+  DEVICE_MODEL := TL-WR841N/ND
+  DEVICE_VARIANT := v12
+  TPLINK_HWID := 0x08410012
+  SUPPORTED_DEVICES += tl-wr841n-v11
+  IMAGES += factory-us.bin factory-eu.bin
+  IMAGE/factory-us.bin := tplink-v1-image factory -C US
+  IMAGE/factory-eu.bin := tplink-v1-image factory -C EU
+endef
+TARGET_DEVICES += tplink_tl-wr841-v12
